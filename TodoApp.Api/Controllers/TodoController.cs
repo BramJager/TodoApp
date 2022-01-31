f6acc99ef3aed9059ad2733716bd2734320c26fa
Bram Jager <bramjager@upcmail.nl>
2022-01-31 09:54:19 +0100

    Add project files.

---
+++ TodoApp.Api/Controllers/TodoController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApp.Business.Interfaces;
+using TodoApp.Domain;
+
+namespace TodoApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodoService todoService;
+
+        public TodoController(ITodoService todoService)
+        {
+            this.todoService = todoService;
+        }
+
+        [HttpGet]
+        [Route("/todos")]
+        public IActionResult GetAll()
+        {
+            return Ok(todoService.GetAll());
+        }
+
+        [HttpGet]
+        [Route("/todo/{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(todoService.GetById(id));
+        }
+
+        [HttpPost]
+        [Route("/todo/create")]
+        public IActionResult Create(Todo todo)
+        {
+            todoService.Create(todo);
+            return Ok(todo);
+        }
+
+        [HttpPost]
+        [Route("/todo/{id}/check")]
+        public IActionResult Check(int id)
+        {
+            var todo = todoService.GetById(id);
+            todo.IsDone = !todo.IsDone;
+            todo = todoService.Update(todo);
+            return Ok(todo);
+        }
+
+        [HttpPost]
+        [Route("/todo/{id}/delete")]
+        public IActionResult Delete(int id)
+        {
+            var todo = todoService.GetById(id);
+            todoService.Delete(todo);
+            return Ok();
+        }
+    }
+}
---
+++ TodoApp.Api/Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using TodoApp.Business;
+using TodoApp.Business.Interfaces;
+using TodoApp.Business.Repositories;
+using TodoApp.Domain;
+
+namespace TodoApp.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("TodoDatabase")));
+
+            services.AddScoped<IRepository<Todo>, EFRepository<Todo>>();
+            services.AddScoped<ITodoService, TodoService>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoApp.Api", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApp.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
